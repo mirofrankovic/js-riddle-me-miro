cb0ca45164d1b9816c016ba50a15b494d9579367
Miro Frankovic <miro.frankovic@gmail.com>
2020-08-13 23:05:54 +0000

    Work with JSON file

--- app.js
+++ app.js
@@ -1,38 +1,29 @@
-const startQuiz = document.getElementById('startGame-btn')
+var questionary
+
+const startQuiz = document.getElementById('startQuiz-btn')
 const quizContainrElement = document.getElementById('quiz-container')
 const questionElement = document.getElementById('startQuestion')
 
 startQuiz.addEventListener('click', startGame)
 
-let questionCounterIndex, currentQuestionIndex     /* the current question in all questions */
+function preload() {
+    questionary = loadJSON(application.json);
+}
+
 
 
 function startGame () {
     console.log('Started now')
-    startQuiz.classList.add('hide')
-    quizContainrElement.classList.remove('hide')
-    startQuestion ()
-    questionCounterIndex = questions.sort()
-    currentQuestionIndex = 0
-}
-
-
-function countQuestion () {
+   
 
 }
 
 function prepareQuestion () {
-    prepareQuestionElement.classList.add()
-     questions = loadJSON ("application.json");
-     showQuestion(questionCounterIndex[currentQuestionIndex])
-    
+   
 }
 
 function showQuestion(question) {
-    questionElement.innerText = question.question
 }
-
-
 
 function attemptNextQuestion () {
 
@@ -45,36 +36,3 @@ function score () {
 function aswers () {
 
 }
-
-
-
-const questions = JSON.parse(application.json);
-
-const data;
-
-
-
-$(document).ready(function () {
-            $.getJSON('application.json', function (loadQuestion) {
-                console.log(loadQuestion);
-            });
-        });
-
-/*
-
-function preload() {
-    data = loadJSON ("application.json");
-}
-/*
-function setup() {
-    noCanvas();
-    var question = data.questionary[0].question[0];
-    createP(question);
-
-}
-console.log(data);
-/*
-fetch("test.json")
-  .then(response => response.json())
-  .then(json => console.log(json));
-*/
